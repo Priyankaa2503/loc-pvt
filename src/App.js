1a1e7b614392dee3035d73de00696531f7a0c75c
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-03-10 18:07:17 +0530

    added signup and login page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import './App.css';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom" ;
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Login from './Login/signup/Login';
-import Signup from './Login/signup/Signup';
-import Home from './pages/Home';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./Login/signup/Login";
+import Signup from "./Login/signup/Signup";
+import Home from "./pages/Home";
 
 function App() {
   return (
-   
-  <div>
-     <Router>
-     
-     <Routes>
-           <Route path = "/" element = {<Home/>}></Route>
-           <Route path = "/signup" element = {<Signup/>}></Route>       
-           <Route path = "/login" element = {<Login/>}></Route>
-           <Route path = "/navbar" element = {<Navbar/>}></Route>
-           <Route path = "/footer" element = {<Footer/>}></Route>
-          
-     </Routes>
-   </Router>
-  </div>
- 
-   
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/navbar" element={<Navbar />}></Route>
+          <Route path="/footer" element={<Footer />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
-  
 }
 
 export default App;
